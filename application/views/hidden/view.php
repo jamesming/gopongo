867a7a8ce9314e1737421ce028e032158230872e
James Ming <jamesming@gmail.com>
2012-09-10 18:23:07 -0700

    added

--- application/views/hidden/view.php
+++ application/views/hidden/view.php
@@ -1,17 +1,47 @@
 ﻿<div   id='modal_box'   style='display:none'  >
-	<div  class='form_asset_container span6' >
+	<div  class='form_asset_container span7' >
 		<form  class="form-horizontal">
-		  <div class="control-group">
-		    <label class="control-label" for="asset_name">Name</label>
-		    <div class="controls">
-		      <input type="text" class="asset_name" placeholder="Name" >
-		    </div>
-		  </div>
-		  <div class="control-group">
-		    <div class="controls">
-		      <button class='btn btn-primary submit_asset_form'  type="button" >Submit</button>
-		    </div>
-		  </div>
+			
+			<h2>Video Upload
+			</h2>
+			<div>
+				<div  class=' oh' >
+					
+					
+					  <div class="control-group">
+					    <label class="control-label" for="asset_name">Name</label>
+					    <div class="controls">
+					      <input type="text" class="asset_name input-medium" placeholder="Name" >
+					    </div>
+					  </div>				
+					  
+					  <div class="control-group">
+					    <label class="control-label" for="asset_description">Description</label>
+					    <div class="controls">
+					    	<textarea class="asset_description input-medium" placeholder="Description" ></textarea>
+					    </div>
+					  </div>		  
+					  
+					
+				</div>
+				
+				
+				<div  class=' oh' >
+					
+					<div  class='img_wrapper ' >
+						<img src="http://www.placehold.it/280x159" class="img-polaroid">
+					</div>
+					<div class="input-append">
+					  <input class="span2" id="appendedInputButton" size="16" type="text"><button class="btn" type="button">Go!</button>
+					</div>			
+					
+				</div>				
+			</div>
+			
+			<div  class='submit_wrapper '   >
+				<button class='btn btn-primary submit_asset_form'  type="button" >Submit</button>
+			</div>
+		  
 		</form>
 	</div>
 </div>
