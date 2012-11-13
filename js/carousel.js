a71fc81b86ebeb8b7bb5ba42d2ec10ed270c989f
James Ming <jamesming@gmail.com>
2012-11-12 17:16:02 -0800

    added

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,33 @@
 ﻿_.extend(core, {
 	start:function(){
 		
-		
 		var  that = this
 			,url = window.base_url  + 'index.php/ajax/getCarousel';
 		
 		$('#json').load(url, function(){
-			
-			console.log(JSON.stringify(core.carousel));
-			
+			that.addImagesToCarousel();
+			$('.carousel').carousel({
+			  interval: 2000
+			})			
 		});	
+
+	},
+	
+	addImagesToCarousel: function(){
 		
+		var slides = '', tpl = '', img_src ='';
+		this.carousel_tpl = core.loadTemplate(window.base_url+'js/tpl/carousel.tpl');
 		
-		$('.carousel').carousel({
-		  interval: 2000
-		})
+		for(var idx in core.carousel){
+			
+			tpl  = this.carousel_tpl;
+			img_src = window.base_url + 'uploads/'+ core.carousel[idx].asset_id +'/thumb/image.jpg?v=' + Math.random();		
+			tpl = tpl.replace(/{{img_src}}/g, img_src);
+			
+			slides += tpl;
+		};
+		
+		$('.carousel-inner').append(slides);		
 	}
 });
 
