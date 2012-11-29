68963a9a690e56b845bd0224830ef6b750322614
James Ming <jamesming@gmail.com>
2012-11-29 13:58:18 -0800

    added

--- application/views/footer/company.php
+++ application/views/footer/company.php
@@ -1,3 +1,19 @@
+<style>
+#footBng{
+	position:absolute;
+	bottom:0px;
+	height:184px;
+	width:100%;
+	background:url(http://localhost/gopongo/img/footerTiled.png) repeat-x;
+	-webkit-box-sizing: border-box;	
+	-moz-box-sizing: border-box;
+	-o-box-sizing: border-box;
+	box-sizing: border-box;
+	padding-top:20px;
+}
+</style>
+<div  id='footBng' >
       <footer  class='container ' >
         <p>&copy; <?php echo $company_name    ?> 2012</p>
       </footer>
+</div>
