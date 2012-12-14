b7c77ef7a514bafee4b281dc48bd8b18db5d6a05
James Ming <jamesming@gmail.com>
2012-12-14 10:27:38 -0800

    added

--- application/views/footer/company.php
+++ application/views/footer/company.php
@@ -1,5 +1,5 @@
 <div  id='footBng'    >
       <footer  class='container ' >
-        <p>&copy; <?php echo $company_name    ?> 2012</p>
+        <p  class='Gotham ' >3301 Barham blvd * Los Angeles * CA 90068 LA <span  class='telephone GothamBold' >323*850*3333</span>  NY <span class='telephone GothamBold'>212*262*3333</span>  PONGO@gopongo.com</p>
       </footer>
 </div>
