2c4af03bb019dd0b3129aefa047c7bd026dc2000
James Ming <jamesming@gmail.com>
2012-12-29 12:13:51 -0800

    added

--- application/views/body/main/testvideo.php
+++ application/views/body/main/testvideo.php
@@ -1,13 +1,4 @@
 
-<script src="http://vjs.zencdn.net/3.2/video.js"></script>
-<video id=home_video1 class="video-js vjs-default-skin" controls preload=none width=640 height=264
-    poster="http://video-js.zencoder.com/oceans-clip.jpg">
-  <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4'/>
-  <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm'/>
-  <track kind=captions src="/video-js/captions.vtt" srclang=en label=English />
-</video>
-
-<script>var homePlayer=_V_("home_video1");</script>
 
 
 <script src="http://vjs.zencdn.net/3.2/video.js"></script>
