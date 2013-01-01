dcf32f01e646b8c3661a643fd88d217dfacd668a
James Ming <jamesming@gmail.com>
2013-01-01 12:31:11 -0800

    added

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -1,6 +1,6 @@
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=8">
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
 	<title><?php echo $title    ?></title>
 	<meta name="description" content="">
 	<meta name="author" content="">
