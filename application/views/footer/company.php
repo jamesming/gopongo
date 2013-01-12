e23045d8070dc478cff2e669f9003a19d17ba27e
James Ming <jamesming@gmail.com>
2013-01-12 15:15:30 -0800

    added

--- application/views/footer/company.php
+++ application/views/footer/company.php
@@ -4,9 +4,21 @@ background: transparent url(<?php echo base_url()    ?>img/footerA.png) no-repea
 height: 26px;
 width: 884px;
 }
+#footBng .container #email_hotspot{
+position: absolute;
+top: 0px;
+left: 710px;
+width: 180px;
+height: 25px;
+cursor:pointer;
+}
 </style>
 
 <div  id='footBng'    >
       <footer  class='container ' >
+      	<a href='mailto:Pongo@gopongo.com' target='_blank'>
+      		<div  id='email_hotspot' >
+      		</div>
+    	</a>
       </footer>
 </div>
