d09c223d12efc39a0a85f8f864734bbc61e45e7e
James Ming <jamesming@gmail.com>
2013-03-17 08:35:35 -0700

    added

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<meta name ="viewport" content ="width = 320">
+	<meta name="viewport" content="width=360">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />
 	<script type="text/javascript" language="Javascript">
 		
